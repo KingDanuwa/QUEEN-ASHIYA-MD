f4f2262feb6cbf7e133608fc10d48c664966fac8
KingDanuwa <110454832+KingDanuwa@users.noreply.github.com>
2022-08-04 07:54:45 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -22,25 +22,25 @@ global.APIKeys = {
 
 //customize settings\\
 
-global.owner = ['94786746412'] //your owner number
-global.premium = ['94786746412'] //your Owner number in menu msg
-global.ownernomer = '94786746412' //your owner number info
+global.owner = ['94787811464'] //your owner number
+global.premium = ['94787811464'] //your Owner number in menu msg
+global.ownernomer = '94787811464' //your owner number info
 
 global.ownername = 'Ishan Sandeepa' //your owner name
-global.botname = '💞 𝚀𝚄𝙴𝙴𝙽 𝙰𝚂𝙷𝙸𝚈𝙰 💞' //your bot name    // ඔයා බොට්ගේ නම නම් වෙනස් කලාට වැඩක් නෑ... බොට්ගේ නම ( 💞 𝚀𝚄𝙴𝙴𝙽 𝙰𝚂𝙷𝙸𝚈𝙰 💞 ) තමයි...
-global.footer = '💞 𝚀𝚄𝙴𝙴𝙽 𝙰𝚂𝙷𝙸𝚈𝙰 💞' 
+global.botname = '💞 𝐌𝐑.𝐒𝐇𝐄𝐍𝐔 💞' //your bot name    // ඔයා බොට්ගේ නම නම් වෙනස් කලාට වැඩක් නෑ... බොට්ගේ නම ( 💞 𝚀𝚄𝙴𝙴𝙽 𝙰𝚂𝙷𝙸𝚈𝙰 💞 ) තමයි...
+global.footer = '💞 𝐌𝐑.𝐒𝐇𝐄𝐍𝐔 💞' 
 
 global.ig = 'https://github.com/ishansandeepa757' //Github Account!
 global.region = 'Sri Lanka'
 global.sc = 'https://github.com/ishansandeepa757'
 global.myweb = 'https://www.youtube.com/c/SinhanadaMusicRemix' //your chanal link
-global.packname = '💞 𝚀𝚄𝙴𝙴𝙽 𝙰𝚂𝙷𝙸𝚈𝙰 💞'
+global.packname = '💞 𝐌𝐑.𝐒𝐇𝐄𝐍𝐔 💞'
 
 global.alivemsgimage = 'https://wallpaperaccess.com/full/3008776.jpg' // ඔයාගේ Alive massege එකට වැටෙන්න ඕන image එකක Link එකක් මෙතනට දෙන්න...
 
 global.chatbotpre = "true"
 
-global.author = '𝘊𝘳𝘦𝘢𝘵𝘦𝘥 𝘉𝘺:- 𝘐𝘴𝘩𝘢𝘯 𝘚𝘢𝘯𝘥𝘦𝘦𝘱𝘢'
+global.author = '𝘊𝘳𝘦𝘢𝘵𝘦𝘥 𝘉𝘺:- 𝐌𝐑.𝐒𝐇𝐄𝐍𝐔'
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']                          // 👈 මේවා ඔයාට ඕන විදියට වෙනස් කර ගන්න \\ 
 global.sp = '⭔'
@@ -50,7 +50,7 @@ global.mess = { // COSTOMIZE WITH YOUR LANGUAGE .....👇
 	
     success: 'හරි ✅',
     admin: 'උබට බෑ බං ඇඩ්මින්ට විතරයි පුලුවන්...!',
-    botAdmin: 'ඔයා ඇඩ්මින් කෙනෙක් නෙවෙයි... !',
+    botAdmin: 'මන් ඇඩ්මින් කෙනෙක් නෙවෙයි... !',
     owner: 'මට විතරයි මේක කරන්න පුලුවන්!',
     group: 'Groups වල විතරයි පුලුවන්...!',
     private: 'Private Chat වල විතරයි පුලුවන්...!',
